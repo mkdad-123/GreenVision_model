2b91d047bea2c85e7c4efdf5257eedac79e80448
mkdad-123 <ma0109424@gmail.com>
2025-09-06 05:56:15 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, UploadFile, File
+from model import load_model, predict_image
+from PIL import Image
+import io
+
+class_names = [
+    'Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
+    'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy',
+    'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 
+    'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot',
+    'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy',
+    'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy',
+    'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight',
+    'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy',
+    'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy',
+    'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight',
+    'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite',
+    'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
+    'Tomato___healthy'
+]
+
+app = FastAPI()
+
+model = load_model("best_model.pth")
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    # قراءة الصورة
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    
+    # prediction (top-3)
+    result = predict_image(image, model, class_names, topk=3)
+    
+    return {"predictions": result}
---
+++ model.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torchvision import models
+from PIL import Image
+import torch.nn.functional as F
+
+
+num_classes = 38  
+
+def load_model(weights_path="best_model18.pth"):
+    model = models.resnet18(pretrained=False)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    weights = torch.load(weights_path , map_location="cpu")
+    model.load_state_dict(weights['model_state_dict'])
+    model.eval()
+    return model
+
+transform = transforms.Compose([
+    transforms.Resize((160,160)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+
+
+def predict_image(image: Image.Image, model, class_names, topk=3):
+    img_tensor = transform(image).unsqueeze(0)  # batch size = 1
+    with torch.no_grad():
+        outputs = model(img_tensor)
+        probs = F.softmax(outputs, dim=1)  
+        top_probs, top_idxs = probs.topk(topk, dim=1)  
+        
+    results = []
+    for i in range(topk):
+        results.append({
+            "class": class_names[top_idxs[0][i].item()],
+            "probability": round(100 * float(top_probs[0][i].item()), 2),
+            "id" : top_idxs[0][i].item(),
+        })
+    
+    return results
+
